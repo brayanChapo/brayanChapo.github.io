321086cf7cc2f6ae977533a2743aaef37578a74f
BrayanChap <cbrayanchap@gmail.com>
2021-05-24 00:36:38 -0500

    quitar cred

--- forms/contact.php
+++ forms/contact.php
@@ -25,12 +25,12 @@
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
 
-  $contact->smtp = array(
-    'host' => 'smtp.live.com',
-    'username' => 'am_interiores@outlook.com',
-    'password' => 'Mirko17/12/2012',
-    'port' => '587'
-  );
+  // $contact->smtp = array(
+  //   'host' => 'smtp.live.com',
+  //   'username' => 'xxxxx@outlook.com',
+  //   'password' => 'xxxxxx',
+  //   'port' => '587'
+  // );
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
